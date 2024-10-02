47ffd776f10550bd3f5ba56f6c30d152750d6056
livinath <159555418+livinath@users.noreply.github.com>
2024-10-02 07:57:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,62 @@
+criaCartao(
+    'Programação',
+    'O que é uma senha forte?',
+    'manter suas informações pessoais em segurança; proteger seus e-mails, arquivos e demais conteúdos; impedir que outra pessoa entre na sua conta.'
+)
+
+criaCartao(
+    'Filosofia',
+    'qual o conceito da filosofia?',
+    'significa “amor à ssabedoria”.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o estopim da primeira guerra mundial',
+    'o assassinato do arquiduque Francisco Ferdinando.'
+
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz dinheiro em Inglês?',
+    'Oi em ingles é MONEY (MÔNEI)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o conceito de território?',
+    'define um espaço estabelecido por relações de poder e delimitado por fronteiras.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem foi o primeiro presidente do brasil?',
+    'Deodoro da Fonseca.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual foi o estopim da segunda guerra mundial?',
+    'a invasão da Polônia realizada pelos alemães, em setembro de 1939.'
+
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz pé em Inglês?',
+    'Oi em ingles é FEET (FIIT)'
+)
+
+criaCartao(
+    'Português',
+    'Oque é um verbo?',
+    'significado de ação, estado, mudança de estado ou fenômeno da natureza, e que variam em relação ao tempo.'
+
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Oi em ingles é GOODBYE (GUDBAI)'
+)
